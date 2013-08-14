17e47cc68f289e7626f8d22794d0696c4bcc4a06
Dalai Felinto <dfelinto@gmail.com>
2013-08-14 18:28:19 -0300

    adding an option to automatically calculate the image hash

--- scripts/texture_imageswapper.py
+++ scripts/texture_imageswapper.py
@@ -47,12 +47,13 @@ from bpy.app.handlers import persistent
 from bpy.props import BoolProperty, StringProperty, IntProperty
 import os
 
+
 class DATA_PT_image_swapper(bpy.types.Panel):
     bl_label = "Image Swapper"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
     bl_context = "texture"
-    
+
     @classmethod
     def poll(cls, context):
         if not context.texture or context.texture.type != 'IMAGE':
@@ -62,54 +63,92 @@ class DATA_PT_image_swapper(bpy.types.Panel):
     def draw_header(self, context):
         texture = context.texture
         self.layout.prop(texture, "image_animate", text="")
-    
+
     def draw(self, context):
         layout = self.layout
         texture = context.texture
-        
+
         col = layout.column()
         col.active = texture.image_animate
-        col.prop(context.texture, "image_hash")
-        col.prop(context.texture, "image_slide")
+        col.prop(texture, "image_slide")
+        col.prop(texture, "use_custom_hash")
+        if texture.use_custom_hash:
+            col.prop(texture, "image_hash")
+
+
+def customHash(self, context):
+    '''changes the custom hash option'''
+    if self.image and           \
+       self.image.filepath != "":
+
+        if self.use_custom_hash:
+            self.image_hash = calculateHash(self.image.filepath)
+
 
 def slideUpdate(self, context):
     '''updates'''
     # extract properties from the texture object
-    if self.image_animate and self.image_hash != "" and self.image and self.image.filepath != "":
-        imageChanger(self, self.image_slide, self.image_hash)
+    if self.type == 'IMAGE' and       \
+       self.image_animate and         \
+       self.image and                 \
+       self.image.filepath != "":
+
+        if self.use_custom_hash and self.image_hash != "":
+            image_hash = self.image_hash
+        else:
+            image_hash = calculateHash(self.image.filepath)
+
+        imageChanger(self, self.image_slide, image_hash)
+
+
+def calculateHash(filepath):
+    '''copy from blender code
+       it's the same calculation
+       that goes on when using a
+       sequence of images'''
+    import re
+    pattern = re.compile('[0-9]+')
+    match = re.search(pattern, filepath)
+
+    if match:
+        start,end = match.span()
+        return "%s%%0%dd%s" % (filepath[:start], end - start, filepath[end:])
+
 
 def imageChanger(texture, image_slide, image_hash):
     ''''''
-    if texture.type != 'IMAGE': return
-    
     image = texture.image
-    
+
     # it needs to be absolute otherwise it fails on windows
     filepath = bpy.path.abspath(image.filepath)
     if filepath != image.filepath:
         relative = True
     else:
         relative = False
-    
+
     basedir = os.path.dirname(filepath)
     filename = image_hash % image_slide
-    
+
     filepath = os.path.join(basedir, filename)
-    
+
     if relative:
         filepath = bpy.path.relpath(filepath)
-    
+
     image.filepath = filepath
 # image automatically reloaded
+
 
 @persistent
 def update_textures(context):
     for tex in bpy.data.textures:
         if tex.image_animate:
+            # hack to force call of python update function
             tex.image_slide = tex.image_slide
+
 
 def register_callbacks():
     bpy.app.handlers.frame_change_post.append(update_textures)
+
 
 def unregister_callbacks():
     bpy.app.handlers.frame_change_post.remove(update_textures)
@@ -120,29 +159,37 @@ def register():
         name="Animate",
         default=False,
         description="Animate texture",
-        update=slideUpdate)												 
-        
+        update=slideUpdate)
+
+    bpy.types.Texture.use_custom_hash = BoolProperty(
+        name="Custom Hash",
+        default=False,
+        description="Customize how the formatting of the other frames",
+        update=customHash)
+
     bpy.types.Texture.image_hash = StringProperty (
         name="Hash",
         default="%03d.png",
         description="String formatting for the filename. Use python notation (e.g. \"img-%03d.png\" will be \"img-003.png\" or \"img-9999.png\")",
         update=slideUpdate)
-            
+
     bpy.types.Texture.image_slide = IntProperty(
         name="Slide",
         min=0,
         subtype='UNSIGNED',
         description="Slide number to use in this frame",
         update=slideUpdate)
-        
+
     bpy.utils.register_module(__name__)
     register_callbacks()
+
 
 def unregister():
     del bpy.types.Texture.image_hash
     del bpy.types.Texture.image_slide
     bpy.utils.unregister_module(__name__)
     unregister_callbacks()
+
 
 if __name__ == "__main__":
     register()
